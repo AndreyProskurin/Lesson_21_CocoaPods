5b6eaba484c81e57704f23dd2817201a38130404
Andrey Proskurin <andrewproskurin@gmail.com>
2017-10-24 19:50:41 +0300

    Empty single view app has been added

---
+++ Lesson_21_CocoaPods/Lesson_21_CocoaPods/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Lesson_21_CocoaPods
+//
+//  Created by Andrey Proskurin on 24.10.17.
+//  Copyright © 2017 Andrey Proskurin. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ Lesson_21_CocoaPods/Lesson_21_CocoaPods/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  Lesson_21_CocoaPods
+//
+//  Created by Andrey Proskurin on 24.10.17.
+//  Copyright © 2017 Andrey Proskurin. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
